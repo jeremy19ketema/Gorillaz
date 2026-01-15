c51a450c7fafcf8c41d7a4bee63b26486262afd1
Jeremiah <jeremy19ketema@gmail.com>
2026-01-15 15:54:58 +0300

    funfact

--- Gorilaz.js
+++ Gorilaz.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
 var heroImage = document.querySelector("header img");
     if (heroImage) {
@@ -7,5 +6,18 @@ var heroImage = document.querySelector("header img");
             heroImage.style.transition = "opacity 2s";
             heroImage.style.opacity = "1";
         }, 300);
-    }
+
+var facts = [
+        "Murdoc is evil... but kinda funny",
+        "2-D thinks pineapple belongs on pizza",
+        "Noodle is the coolest 10-year-old ever",
+        "Russel has a ghost friend inside him"
+    ];
+
+    var factElement = document.createElement("p");
+    factElement.style.textAlign = "center";
+    factElement.style.fontStyle = "italic";
+    factElement.style.color = "#777";
+    factElement.style.margin = "30px"; 
+   }
 }
