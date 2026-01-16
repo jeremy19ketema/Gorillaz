6ee63ee587fbb23a34dc6e81ac5da7155f45fa45
Jeremiah <jeremy19ketema@gmail.com>
2026-01-16 21:36:29 +0300

    fix

--- MusicPage.js
+++ MusicPage.js
@@ -1,8 +1,6 @@
 window.onload = function () {
 
     document.body.style.opacity = "1";
-
-   
     var videos = document.querySelectorAll("video");
     videos.forEach(function(video) {
         video.addEventListener("play", function() {
@@ -13,4 +11,19 @@ window.onload = function () {
             });
         });
     });
+    var links = document.querySelectorAll("nav a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", function(e) {
+            if (this.href !== window.location.href) {
+                e.preventDefault();
+                var goTo = this.href;
+
+                document.body.style.opacity = "0";
+
+                setTimeout(function() {
+                    window.location.href = goTo;
+                }, 800);
+            }
+        });
+    }
 };
